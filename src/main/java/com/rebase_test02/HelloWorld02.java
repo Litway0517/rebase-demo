7e26f21cd29300921fd1fdfc9a0342bb4c7e7e8c
Mingw0717-Dell <xmw0517@gmail.com>
2021-08-27 22:57:54 +0800

    master commit 3.7

--- src/main/java/com/rebase_test02/HelloWorld02.java
+++ src/main/java/com/rebase_test02/HelloWorld02.java
@@ -12,9 +12,10 @@ public class HelloWorld02 {
 
     public static void main(String[] args) {
 
-        System.out.println("develop 新 分支第一次提交");
-        System.out.println("develop 新 分支第二次提交");
-        System.out.println("develop 新 分支第三次提交");
-        System.out.println("develop 新 分支第四次提交");
+        System.out.println("master 新 分支第一次提交");
+        System.out.println("master 新 分支第二次提交");
+        System.out.println("master 新 分支第三次提交");
+        System.out.println("master 新 分支第四次提交");
+        System.out.println("master 新 分支第五次提交");
     }
 }
