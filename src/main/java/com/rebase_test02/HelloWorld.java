e758ea52ebb4bd2b7778c9b7168dff91825afd43
Mingw0717-Dell <xmw0517@gmail.com>
2021-08-27 22:30:25 +0800

    master commit 3.1

---
+++ src/main/java/com/rebase_test02/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.rebase_test02;
+
+/**
+ * 你好，世界
+ *
+ * @author DELL_
+ * @date 2021-08-27
+ */
+
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("master 新 分支第一次提交");
+    }
+}
