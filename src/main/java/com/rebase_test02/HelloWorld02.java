bfd34c451d42f99daf9a385f741a5df9193886bf
Mingw0717-Dell <xmw0517@gmail.com>
2021-08-27 22:33:45 +0800

    develop commit 2.2

--- src/main/java/com/rebase_test02/HelloWorld02.java
+++ src/main/java/com/rebase_test02/HelloWorld02.java
@@ -13,5 +13,6 @@ public class HelloWorld02 {
     public static void main(String[] args) {
 
         System.out.println("develop 新 分支第一次提交");
+        System.out.println("develop 新 分支第二次提交");
     }
 }
